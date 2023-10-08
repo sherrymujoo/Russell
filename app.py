90312c7a5d7db428b1284072e37d0ba8fbe6e096
Sharan <130827526+sherrymujoo@users.noreply.github.com>
2023-10-08 16:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 import os
 from dotenv import load_dotenv
 load_dotenv()
-openai.api_key = "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"
+openai.api_key = "  " // enter your openai api-key
 app = Flask(__name__)
 
 
@@ -33,38 +33,24 @@ the relevance of the question/ For any URL's please embed them as hyperlinks so 
 Professional Summary: 
 I'm a Generalist PM over 6 years of experience across various facets of Product Management. My diverse background as a Software Engineer, Program Manager, Qualitative Market Researcher, Project Manager, NPS Analyst, and Product Owner has given me a unique perspective on how organizations work. I excel at both paying attention to the details and keeping track of the big picture.
 
-- My roles have taught me the importance of empathy, both for customers and the business. I've found that sincerity and persistence are key to solving tough problems. I believe that perfection is a journey, not a destination, and that a design process is only as strong as the values of the team members.
-
-- I thrive in challenging environments that push me out of my comfort zone. I'm not afraid to be the dumbest person in the room, because I know that it will drive me to grow and improve.
 
 Employment Particulars:
-- Current Location: Bengaluru
-- Ready to Relocate: Mostly looking for Bangalore based opportunities but open to hearing about other locations
-- Reason for Change: Organization Restructuring
-- Notice Period: Not serving a notice period . Immediately Available. 
-- Work Preference: Hybrid / Open to commuting
-- Work Samples and Resume , available at the following link : ( https://drive.google.com/drive/folders/1aZ_541LLAAOMBk4_jSOWMzahHh0x0Zhe?usp=drive_link )
-- For booking a call : https://calendly.com/sherrymujoo/15min
+- Current Location:
+- Ready to Relocate: 
+- Reason for Change: 
+- Notice Period: 
+- Work Preference:
+- Work Samples and Resume , available at the following link : 
+- For booking a call : https://calendly.com/
 
 Product Management Experience:
-I worked as the Product Owner for Entitlements Management, a new initiative at the intersection of feature management and software monetization. Entitlements helps you objectively understand which features delivery value to your customers and revenue for your business.
-For a detailed walkthrough , the following URL is a video explainer of Sharan's experience as a PM. ( https://www.loom.com/share/8efa343efc1b46c5a3037180d29ef519?sid=e1b12a3a-0f0a-4ad5-8ad8-c7f1ff7f5eda )
-Key Results:
-- Increased adoption 3x in half the time for a new initiative by using a multi-pronged approach.
-- Transitioned the new module from BETA to General availability with the help of engineering and design.
-- Implemented Email Campaign, Feature Videos and In-app messaging to drive awareness about value proposition and future roadmap.
-- Laid out API and webhook specifications targeted at reducing time to value OR aha moment by significantly reduce implementation effort.
-- Conducted multiple use case training sessions for Solutions Consultants, Implementation Consultants to democratize implementation efforts and increase feature awareness within the org.
+- I did x, y,z
 
 Responsibilities:
 - Performed Market and Customer research to understand what pains can be alleviated and what gains can be made with our product.
-- Defined and prioritized short/mid-term roadmap in close collaboration with Engineering and GTM.
-- Wrote functional and non-functional specifications to anchor the design and development of features for UI and API.
-- Worked with marketing to define and iterate on messaging, positioning and overall storytelling.
-- Developed and maintained a clean, healthy backlog in JIRA.
-- Conducted Grooming for new stories developed.
+- 
 
-Skills: Scrum, A/B Testing, Wireframing, Product Management, User Acceptance Testing, Team Management, Jira, Figma (Software), Sketch App, Stakeholder Management, Business Analysis, Marketing, Sales, Product Road Mapping, Backlog Management, Positioning (Marketing), Product Marketing, User Stories, User Experience (UX).
+Skills:  User Acceptance Testing, Team Management, Jira, Figma (Software), Sketch App, Stakeholder Management, Business Analysis, Marketing, Sales, Product Road Mapping, Backlog Management, Positioning (Marketing), Product Marketing, User Stories, User Experience (UX).
 
 Recruiter and Hiring Manager Queries: 
 Primary categories: Professional Summary, Employment Particulars, Product Management Experience, Skills, Personal Traits, or Work Style.
